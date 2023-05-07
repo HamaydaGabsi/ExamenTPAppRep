d0c323a9f034eac34135c0906020730e78b5392a
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2023-05-07 15:25:08 +0100

    Edit working

--- BO1_server.py
+++ BO1_server.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pika
 import json
+import time
 from CustomJSONEncoderDecoder  import CustomJSONEncoder;
 
 # Connect to the BO database
@@ -33,21 +34,16 @@ def send_sales_data():
 
     # Bind the queue to the exchange
     channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
-    # Fetch the latest sales data from the BO database
     # Fetch the latest sales data from the BO database where isSync is False
     cursor = bo1_db.cursor()
     query = "SELECT * FROM sales WHERE isSync = 0 ORDER BY sale_date DESC LIMIT 10"
     cursor.execute(query)
     rows = cursor.fetchall()
     rows.append("1")
-
     cursor.close()
 
-
-   # Convert the rows to a JSON string and send it to the HO database
+    # Convert the rows to a JSON string and send it to the HO database
     message = json.dumps(rows, cls=CustomJSONEncoder)
-    print(message)
-
     # Publish the message to RabbitMQ
     channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=message)
 
@@ -64,4 +60,3 @@ def send_sales_data():
     connection.close()
 
 # Start the sending loop
-
--- FillDB.py
+++ FillDB.py
@@ -25,11 +25,11 @@ cursor = cnx.cursor()
 #         ('Product B', '2023-05-02'), \
 #         ('Product C', '2023-05-02'), \
 #         ('Product D', '2023-05-03')")
-# queries.push(" INSERT INTO sales (product_name, sale_date) VALUES \
-#         ('Product 1', '2023-05-01'), \
-#         ('Product 2', '2023-05-02'), \
-#         ('Product 3', '2023-05-02'), \
-#         ('Product 4', '2023-05-03')")
+query =(" INSERT INTO sales (product_name, sale_date) VALUES \
+        ('Product 1', '2023-05-01'), \
+        ('Product 2', '2023-05-02'), \
+        ('Product 3', '2023-05-02'), \
+        ('Product 4', '2023-05-03')")
 # queries.push("ALTER TABLE sales ADD COLUMN isSync BOOLEAN NOT NULL DEFAULT FALSE;")
 # queries.push( "CREATE TRIGGER sync_sales\
 #           AFTER UPDATE ON sales\
@@ -39,13 +39,15 @@ cursor = cnx.cursor()
 #             SET NEW.isSync = FALSE;\
 #           END IF;\
 #         END;")
-query="DELIMITER //\
-      CREATE TRIGGER set_sync_false BEFORE UPDATE ON sales\
-      FOR EACH ROW\
-      BEGIN\
-        SET NEW.isSync = false;\
-      END//\
-      DELIMITER ;"
+# query="DELIMITER //\
+#       CREATE TRIGGER set_sync_false BEFORE UPDATE ON sales\
+#       FOR EACH ROW\
+#       BEGIN\
+#         IF NEW.product_name <> OLD.product_name OR NEW.sale_date <> OLD.sale_date THEN\
+#               SET NEW.isSync = 0;\
+#           END IF;\
+#       END//\
+#       DELIMITER ;"
 
 
 print(query)
